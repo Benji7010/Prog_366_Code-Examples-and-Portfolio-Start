f9f1068ee02ea5a6b4c6703893e74ac98d65f89c
Benji7010 <benjialwaysthere@gmail.com>
2025-10-05 20:29:20 -0500

    fish

--- Program.cs
+++ Program.cs
@@ -1,13 +1,63 @@
-﻿namespace Prog_405_Code_Examples_and_Portfolio_Start
+﻿using System;
+using System.IO;
+
+namespace Prog_405_Code_Examples_and_Portfolio_Start
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine(RotateConstant(45, 90));
-            Console.WriteLine(BigDifference(new int[] { 2, 4 }));
-            Console.WriteLine(BigDifference(new int[] {int.MinValue, int.MaxValue}));
-            Console.WriteLine(uint.MaxValue);
+            //Console.WriteLine(RotateConstant(45, 90));
+            //Console.WriteLine(BigDifference(new int[] { 2, 4 }));
+            //Console.WriteLine(BigDifference(new int[] {int.MinValue, int.MaxValue}));
+            //Console.WriteLine(uint.MaxValue);
+            string[] shuff = FishShuff();
+            foreach (string s in shuff)
+                Console.WriteLine(s);
+        }
+
+        static string[] FishShuff()
+        {
+            List<string> shuff = GetFile();
+
+            Random rand = new Random();
+
+            for (int i = 0; i < shuff.Count; i++)
+            {
+                int gen = rand.Next(i, shuff.Count);
+                string temp = shuff[i];
+                shuff[i] = shuff[gen];
+                shuff[gen] = temp;
+            }
+
+            return shuff.ToArray();
+        }
+
+        private static List<string> GetFile()
+        {
+            // Keeping it simple, not great with serialization so a modified off the shelf solution when the requirements are still flexibe seems pretty good.
+            
+            //Console.WriteLine("FilePath: ");
+            //string? file = Console.ReadLine();
+            string file = "C:/Users/benji/Downloads/testFile.txt";
+
+            if (File.Exists(file))
+            {
+                StreamReader Textfile = new StreamReader(file);
+                string line;
+                // TODO: Find a type of manipulatable list that doesn't have sorting and shuffling built in.
+                List<string> lines = new List<string>();
+
+                while ((line = Textfile.ReadLine()) != null)
+                {
+                    lines.Add(line);
+                }
+
+                Textfile.Close();
+
+                return lines;
+            }
+            else { throw new FileNotFoundException(); }
         }
 
         //Constant Time. Will only run a limited amount of operations even with numbers below 0.
--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# Assignment.
+# Assignments
+## Fish
+I implimented fish shuffle that shuffles a list of strings separated by new lines. Then it prints to console.
+You know what that means.
