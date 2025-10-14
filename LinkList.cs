a84ed227d6ae9eddc5184387d83f45349285dc28
Benji7010 <385421@colum.edu>
2025-10-14 18:19:21 -0500

    2025-10-14

--- LinkList.cs
+++ LinkList.cs
@@ -6,19 +6,52 @@ using System.Threading.Tasks;
 
 namespace Prog_405_Code_Examples_and_Portfolio_Start
 {
-    public class Node<T>
+    internal class Node<T>
     {
-        public T? Value {  get; set; }
+        public T? Value { get; set; }
         public Node<T>? Next { get; set; }
+    }
+
+    public interface IIterator<T>
+    {
+        //
+        public T Next();
+        public bool HasNext();
+    }
+
+    public class Iterator<T> : IIterator<T>
+    {
+        LinkedList<T> List { get; set; }
+
+        public Iterator(LinkedList<T> list)
+        {
+            List = list;
+        }
+        
+        public bool HasNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public T Next()
+        {
+            
+        }
     }
 
     public class LinkedList<T>
     {
-        Node<T>? node;
+        Node<T>? FirstNode { get; set; }
 
-        public void Append(T data)
+        //Add a node to the front of list.
+        public void Shove(T data)
         {
-            int.MaxValue
+            FirstNode = new Node<T> { Value = data, Next = FirstNode };
+        }
+
+        public T ElementAt(int index)
+        {
+            throw new NotImplementedException();
         }
     }
 }
