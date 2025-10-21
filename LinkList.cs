cc343d6af75c3c3f782f59361ef5f4a6212846f5
Benji7010 <385421@colum.edu>
2025-10-21 18:20:40 -0500

    2025-10-21

--- LinkList.cs
+++ LinkList.cs
@@ -3,27 +3,69 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace Prog_405_Code_Examples_and_Portfolio_Start
 {
     internal class Node<T>
     {
-        public T? Value { get; set; }
+        public T Value { get; set; }
         public Node<T>? Next { get; set; }
+
+        public Node(T value)
+        {
+            Value = value;
+        }
     }
 
     public interface IIterator<T>
     {
-        //
-        public T Next();
+        public T? NextValue();
+
         public bool HasNext();
     }
 
     public class Iterator<T> : IIterator<T>
     {
         LinkedList<T> List { get; set; }
-        public int Count { get; set; }
-        private Node<T> CurrentNode { get; set; }
+        public int Count { get; private set; }
+        internal Node<T>? CurrentNode { get; set; }
+        public T? GetCurrentValue
+        {
+            get
+            {
+                if (CurrentNode == null)
+                {
+                    return default(T?);
+                }
+                return CurrentNode.Value;
+            }
+        }
+        public T SetCurrentValue {
+            set
+            {
+                if (CurrentNode != null)
+                {
+                    CurrentNode.Value = value;
+                }
+            }
+        }
+        
+        //Iterate to the next value and return it.
+        public T? NextValue()
+        {
+            if (CurrentNode != null)
+            {
+                if (HasNext())
+                {
+                    CurrentNode = CurrentNode.Next;
+                    Count++;
+                    if (CurrentNode != null)
+                        return CurrentNode.Value;
+                }
+            }
+            return default(T);
+        }
 
         public Iterator(LinkedList<T> list)
         {
@@ -32,55 +74,72 @@ namespace Prog_405_Code_Examples_and_Portfolio_Start
             CurrentNode = List.FirstNode;
         }
 
+        //Is there another node after the current?
         public bool HasNext()
         {
-            if (CurrentNode.Next != null) 
+            if (CurrentNode != null && CurrentNode.Next != null) 
                 return true;
             return false;
-        }
-
-        public T? Next()
-        {
-            CurrentNode = CurrentNode.Next;
-            Count++;
-            return CurrentNode.Value;
         }
     }
 
     public class LinkedList<T>
     {
         internal Node<T>? FirstNode { get; set; }
-        
+
         //Add a node to the front of list.
         public void AddFirst(T data)
         {
-            FirstNode = new Node<T> { Value = data, Next = FirstNode };
+            FirstNode = new Node<T>(data) {Next = FirstNode };
         }
 
         //Remove node at the front of the list.
         public void RemoveFirst()
         {
-            FirstNode = FirstNode.Next;
+            if (FirstNode != null)  
+                FirstNode = FirstNode.Next;
         }
         
+        //Add node to the end of the list.
         public void AddLast(T data)
         {
-            LastNode.Next = new Node<T> { Value = data, Next = null };
-            LastNode = LastNode.Next;
+            if(FirstNode == null)
+            {
+                AddFirst(data);
+                return;
+            }
+            Iterator<T> iterator = new Iterator<T>(this);
+            while (iterator.CurrentNode.Next != null)
+            {
+                iterator.NextValue();
+            }
+            Node<T>? lastNode = iterator.CurrentNode;
+            if (lastNode == null)
+            {
+                lastNode = new Node<T>(data);
+            }
+            else
+            {
+                lastNode.Next = new Node<T>(data);
+            }
         }
 
+        //Remove last node from the list.
         public void RemoveLast()
         {
-
-        }
-
-        //Finds Node At Element.
-        public T ElementAt(int index)
-        {
-            for(int i = 0; i <= index; i++)
+            if (FirstNode == null)
             {
-
+                RemoveFirst();
+                return;
             }
+            Iterator<T> iterator = new Iterator<T>(this);
+            Node<T>? previousNode = null;
+            while (iterator.CurrentNode.Next != null)
+            {
+                previousNode = iterator.CurrentNode;
+                iterator.NextValue();
+            }
+            iterator.CurrentNode = previousNode;
         }
     }
 }
